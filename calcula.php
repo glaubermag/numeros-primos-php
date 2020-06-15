0e094cd9a5b94692033b8d110629dbf3cda8a842
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2020-06-15 19:09:59 -0300

    Update calcula.php
    Alterado o algoritmo usando a função gmp next prime nativa do PHP para melhorar a perfomance do código

--- calcula.php
+++ calcula.php
@@ -1,13 +1,24 @@
 <?php
+
+// INFORMAÇÃO IMPORTANTE, PARA O FUNCIONAMENTO DESSE CÓDIGO É NECESSÁRIO TER ATIVADO A BILIOTECA GMP DENTRO DO SERVIDOR QUE IRÁ RODAR O CÓDIGO
+
+//NO CASO DO XAMPP FOI NECESSÁRIO APENAS TIRAR O COMENTÁRIO DA LINHA DENTRO DE PHP.INI
+
+
  // Recebe o valor enviado pelo form e insere dentro da variável calcula
  $calcula = $_POST["variavel"];
 
-  // percorre os números de 1 até $calcula
-for($i = 1; $i <= $calcula; $i++)
+// utilizando a função gmp_nextprime conforme a biblioteca: https://www.php.net/manual/pt_BR/function.gmp-nextprime.php
+
+ for($i = 1; $i <= $calcula; $i++)
 {
-  $prime = gmp_nextprime($calcula);
+  //confere qual o número mais próximo de $i é primo e guarda na varíavel $prime
+  $prime = gmp_nextprime($i);
 
-  if ($prime <= $calcula)
-  echo $i . ', ';
-
+  //se o número primo dentro de $prime estiver dentro do intervalo informado, faz o print do número primo e o insere na varíavel $i para continuar verificando o próximo primo a partir daí, dessa forma
+  // melhorando demais o desempenho do código
+  if ($prime <= $calcula){
+    echo $prime . ', ';
+    $i = $prime;
+  }
 }
