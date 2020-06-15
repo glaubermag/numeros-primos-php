a024efa7f351d1ee989e91df48f93b8fc7791bf8
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2020-06-15 18:52:08 -0300

    alteração no algoritmo

--- calcula.php
+++ calcula.php
@@ -2,21 +2,12 @@
  // Recebe o valor enviado pelo form e insere dentro da variável calcula
  $calcula = $_POST["variavel"];
 
- 
- // percorre os números de 1 até $calcula
+  // percorre os números de 1 até $calcula
 for($i = 1; $i <= $calcula; $i++)
 {
-    $divisores = 0;
-     //pega o número do outro for acima e verifica quantos divisores ele tem
-    for($j = $i; $j >= 1; $j--)
-    {
-        if (($i % $j) == 0) {
-            $divisores++;
-        }
-    }
-    // se só tiver dois divisores, faz o print do número
-    if ($divisores == 2)
-    {
-        echo $i . ', ';
-    }
+  $prime = gmp_nextprime($calcula);
+
+  if ($prime <= $calcula)
+  echo $i . ', ';
+
 }
